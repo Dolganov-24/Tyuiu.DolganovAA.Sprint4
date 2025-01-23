a038446b122d388fe6ed3a4b2e4f24ce827965bf
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2025-01-24 01:05:18 +0300

    Task5

---
+++ Tyuiu.DolganovAA.Sprint4.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.DolganovAA.Sprint4.Task5.V2.Lib;
+namespace Tyuiu.DolganovAA.Sprint4.Task5.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
