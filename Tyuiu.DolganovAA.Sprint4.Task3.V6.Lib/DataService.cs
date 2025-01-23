df4fe66521357209f3d71d5c9e967ef054a5b023
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2025-01-24 00:49:57 +0300

    Task3

---
+++ Tyuiu.DolganovAA.Sprint4.Task3.V6.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.DolganovAA.Sprint4.Task3.V6.Lib
+
+{
+    public class DataService : ISprint4Task3V6
+    {
+        public int Calculate(int[,] array)
+        {
+            int MaxNum = 0;
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                if (array[1, i] > MaxNum)
+                {
+                    MaxNum = array[1, i];
+                }
+            }
+            return MaxNum;
+        }
+    }
+}
