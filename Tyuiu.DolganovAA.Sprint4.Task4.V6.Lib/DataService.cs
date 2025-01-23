dc7f12744f0e25de248cb316561c5915adb6cb88
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2025-01-24 00:54:53 +0300

    Task4

---
+++ Tyuiu.DolganovAA.Sprint4.Task4.V6.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.DolganovAA.Sprint4.Task4.V6.Lib
+{
+    public class DataService : ISprint4Task4V6
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int colums = matrix.GetUpperBound(0) + 1;
+            int rows = matrix.Length / colums;
+
+
+
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    if (matrix[i, j] % 2 != 0)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.DolganovAA.Sprint4.Task4.V6.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.DolganovAA.Sprint4.Task4.V6.Lib;
+namespace Tyuiu.DolganovAA.Sprint4.Task4.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
